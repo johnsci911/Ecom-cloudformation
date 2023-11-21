5d3e0552d90415ed4b42c8ffaaba8f3a7e97a062
jkcachero <johnguitarizta@gmail.com>
2023-11-22 04:24:57 +0800

    update email template to show order summary

--- email_sender/resources/views/emails/order-is-placed.blade.php
+++ email_sender/resources/views/emails/order-is-placed.blade.php
@@ -1,8 +1,16 @@
 <x-mail::message>
-# An order is placed
+# Hi {{ $email->user_email }} your order has been placed
 
-Product: {{ $email->product_name }} has been placed
-
+## Order Summary:
+@component('mail::table')
+    | Product Name                   | Description                           | Price                              |
+    | ------------------------------ | ------------------------------------- | ----------------------------------:|
+    @foreach($cartData as $item)
+    | {{ $item->product_name }}      | {{ $item->product_description }}      | ${{ $item->product_price }}        |
+    @endforeach
+    |                                | Total                                 | ${{ $email->total }}               |
+@endcomponent
 Thanks,<br>
 {{ config('app.name') }}
 </x-mail::message>
+
