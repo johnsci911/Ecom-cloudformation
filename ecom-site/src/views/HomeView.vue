5fc5b1815cd1dbf1b1b7046db682e962e419723e
jkcachero <johnguitarizta@gmail.com>
2023-11-21 18:28:08 +0800

    Sweetalert

--- ecom-site/src/views/HomeView.vue
+++ ecom-site/src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { onMounted, ref } from 'vue'
+import { useFlash } from "@/composables/useFlash"
 
 const products = ref([])
+
+let cart = ref([])
+let { flash } = useFlash()
 
 onMounted(() => {
   fetch('http://18.234.82.38:8000/graphql', {
@@ -34,10 +38,7 @@ onMounted(() => {
     products.value = result.data.products.data
   })
 })
-function showAlert() {
-  this.$swal('Test')
-}
-function checkout(product_id, user_id, user_email, product_name, product_description, product_price) {
+let checkout = (product_id, user_id, user_email, product_name, product_description, product_price) => {
   fetch('http://18.234.82.38:8002/graphql', {
     method: 'post',
     headers: {
@@ -89,26 +90,32 @@ function checkout(product_id, user_id, user_email, product_name, product_descrip
   })
   .then(response => response.json())
   .then(() => {
-    alert('Order is placed')
+    flash('Order is placed')
   })
 }
 </script>
 
 <template>
   <main>
-    <ul class="m-4 grid gap-y-4">
-      <li class="bg-gray-900 p-4 rounded-lg border-none text-white flex flex-row justify-between gap-4" v-for="product in products" :key="product.id">
-        <div class="grid gap-y-6">
-          <h3 class="text-xl">Product: {{ product.name }}</h3>
-          <h4 class="text-sm">{{ product.description }}</h4>
-          <div class="flex flex-row">
-            <strong>Price</strong><p class="ml-3">{{ product.price }}</p>
+    <div class="flex flex-row bg-gray-700">
+      <ul class="bg-gray-800 rounded-lg p-3 m-4 w-3/4 grid gap-y-4">
+        <li class="bg-gray-900 p-4 rounded-lg border-none text-white flex flex-row justify-between gap-4" v-for="product in products" :key="product.id">
+          <div class="grid gap-y-6">
+            <h3 class="text-xl">Product: {{ product.name }}</h3>
+            <h4 class="text-sm">{{ product.description }}</h4>
+            <div class="flex flex-row">
+              <strong>Price</strong><p class="ml-3">{{ product.price }}</p>
+            </div>
           </div>
-        </div>
-        <div>
-          <button class="px-3 py-2 btn bg-green-900 rounded-lg w-32 hover:bg-green-800" @click="checkout(product.id, product.user.id, product.user.email, product.name, product.description, product.price)">Checkout</button>
-        </div>
-      </li>
-    </ul>
+          <div>
+            <button class="px-3 py-2 btn bg-green-900 rounded-lg w-32 hover:bg-green-800" @click="checkout(product.id, product.user.id, product.user.email, product.name, product.description, product.price)">Checkout</button>
+          </div>
+        </li>
+      </ul>
+      <div class="bg-gray-800 my-4 mr-4 rounded-lg w-1/4">
+        <h1 class="text-gray-100 text-2xl p-4">Cart</h1>
+
+      </div>
+    </div>
   </main>
 </template>
