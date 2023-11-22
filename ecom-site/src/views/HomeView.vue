9dd1c16da8801bf06b79cfd121370614709d87b8
jkcachero <johnguitarizta@gmail.com>
2023-11-22 21:46:32 +0800

    Update HomeView.vue

--- ecom-site/src/views/HomeView.vue
+++ ecom-site/src/views/HomeView.vue
@@ -7,6 +7,7 @@ let { flash } = useFlash()
 const products = ref([])
 
 let cart = ref([])
+let email = ref('')
 
 onMounted(() => {
   fetch('http://18.234.82.38:8000/graphql', {
@@ -51,63 +52,68 @@ let removeFromCart = (product_id) => {
 }
 
 let placeOrder = (cartItems, user_id, user_email) => {
-  fetch('http://18.234.82.38:8002/graphql', {
-    method: 'post',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      query: `
-        mutation {
-          createCheckout(
-            user_id: "${user_id}"
-            cart: "\"${JSON.stringify(JSON.stringify(cartItems))}\""
-          ) {
-            id
-            user_id
-            cart
+  if (user_email === '') {
+    flash('Email is required', 'Warning', 'warning')
+  } else {
+    fetch('http://18.234.82.38:8002/graphql', {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: `
+          mutation {
+            createCheckout(
+              user_id: "${user_id}"
+              cart: "\"${JSON.stringify(JSON.stringify(cartItems))}\""
+            ) {
+              id
+              user_id
+              cart
+            }
           }
-        }
-      `
-    })
-  })
-  .then(response => response.json())
-
-  var totalPrice = cartItems.reduce((sum, object) => (eval(`${sum} + ${object.product_price}`)), 0)
-
-  fetch('http://18.234.82.38:8004/graphql', {
-    method: 'post',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      query: `
-        mutation {
-          createEmail(
-            user_id: "${user_id}",
-            cart: "\"${JSON.stringify(JSON.stringify(cartItems))}\"",
-            user_email: "${user_email}",
-            total: "${totalPrice}",
-          ) {
-            id
-            user_id
-            user_email
-            cart
-            total
-            created_at
-            updated_at
-          }
-        }
-      `
-    })
-  })
-  .then(response => response.json())
-  .then(() => {
-    flash('Order is placed')
-      .then(() => {
-        cart.value = []
+        `
       })
-  })
+    })
+    .then(response => response.json())
+
+    var totalPrice = cartItems.reduce((sum, object) => (eval(`${sum} + ${object.product_price}`)), 0)
+
+    fetch('http://18.234.82.38:8004/graphql', {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: `
+          mutation {
+            createEmail(
+              user_id: "${user_id}",
+              cart: "\"${JSON.stringify(JSON.stringify(cartItems))}\"",
+              user_email: "${user_email}",
+              total: "${totalPrice}",
+            ) {
+              id
+              user_id
+              user_email
+              cart
+              total
+              created_at
+              updated_at
+            }
+          }
+        `
+      })
+    })
+    .then(response => response.json())
+    .then((result) => {
+      flash('Order is placed')
+        .then(() => {
+          cart.value = []
+        })
+      console.log(result)
+    })
+  }
 }
 </script>
 
@@ -129,9 +135,12 @@ let placeOrder = (cartItems, user_id, user_email) => {
         </div>
       </div>
       <div class="bg-gray-800 my-4 mr-4 rounded-lg w-1/4">
-        <div class="flex flex-row justify-between">
-          <h1 class="text-gray-100 text-2xl p-4 align-middle">Cart</h1>
-          <button class="text-white text-md px-3 py-2 m-4 bg-green-900 rounded-lg" @click="placeOrder(cart, '1', 'test@user.com')">Place order</button>
+        <div class="flex flex-row justify-between py-4 mx-4">
+          <h2 class="text-gray-100 text-xl align-middle">Cart</h2>
+          <button class="text-white text-md px-3 bg-green-900 rounded-lg" @click="placeOrder(cart, '1', email)">Place order</button>
+        </div>
+        <div class="text-white pb-3 px-4 flex justify-between">
+          <input type="email" v-model="email" class="px-3 py-2 bg-gray-700 rounded w-full" placeholder="Your email">
         </div>
 
         <ul class="bg-gray-800 rounded-lg mx-4 grid gap-y-4">
